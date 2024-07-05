0dc9a8bc520d17d6254ef5d5278198b7447224c8
Anis Mhamid <anesmhamed1@gmail.com>
2024-07-06 00:21:33 +0300

    commit

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,7 @@
 let currentPlayer = 'X';
 let boardState = ['', '', '', '', '', '', '', '',];
-let counter = 0;
+let xCounter = 0;
+let oCounter = 0;
 
 
 
@@ -14,21 +15,24 @@ function insert_X_Y(id) {
 
         let winner = checkWinner();
         if (winner === 'X') {
-            let result = document.querySelector('#result-right');
-            result.innerHTML = `${--counter}`;
+            xCounter++;
+            document.getElementById('result-left').innerHTML = `X - ${xCounter}`;
+        } else if (winner === 'O') {
+            oCounter++;
+            document.getElementById('result-right').innerHTML = `O - ${oCounter}`;
         }
-        else if (winner === 'O') {
-            let result = document.querySelector('#result-left');
-            result.innerHTML = `${++counter}`;
-            //     disableAllClicks();
-            // } else if (boardState.every(cell => cell !== '')) {
-            //     document.querySelector('.result').innerHTML = "לא נצחתם";
-            //     disableAllClicks();
+
+        if (xCounter + oCounter === 9) {
+            document.querySelector('.results').innerHTML = `نتائج اللاعب X: ${xCounter} فوز, اللاعب O: ${oCounter} فوز`;
         } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         }
     }
 }
+
+
+
+
 
 
 
@@ -38,17 +42,10 @@ function checkWinner() {
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
     ];
-
     for (let combination of winningCombinations) {
         let [a, b, c] = combination;
         if (boardState[a] !== '' && boardState[a] === boardState[b] && boardState[b] === boardState[c]) {
-            if (boardState[a] === 'X') {
-                document.getElementById('result-left').innerText = ++counter
-                return boardState[a];
-            } else {
-                console.log('O is winner');
-                return boardState[a];
-            }
+            return boardState[a];
         }
     }
     return null;
@@ -57,42 +54,32 @@ function checkWinner() {
 
 
 function resetGame() {
-    boardState = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+    boardState = ['', '', '', '', '', '', '', '', ''];
     let cells = document.querySelectorAll('.a');
     cells.forEach(cell => {
         cell.innerHTML = '';
     });
-    document.querySelector('.result').innerHTML = '';
     currentPlayer = 'X';
+    xCounter = 0;
+    oCounter = 0;
+    document.getElementById('result-left').innerHTML = 'X - 0';
+    document.getElementById('result-right').innerHTML = 'O - 0';
+    document.querySelector('.results').innerHTML = '';
     enableAllClicks();
 }
 
 
-let disables = {
-    disableA: ['#A'],
-    disableB: ['#A', '#B'],
-    disableC: ['#A', '#B', '#C'],
-    disableD: ['#A', '#B', '#C', '#D'],
-    disableE: ['#A', '#B', '#C', '#D', '#E'],
-    disableF: ['#A', '#B', '#C', '#D', '#E', '#F'],
-    disableG: ['#A', '#B', '#C', '#D', '#E', '#F', '#G'],
-    disableH: ['#A', '#B', '#C', '#D', '#E', '#F', '#G', '#H'],
-    disableI: ['#A', '#B', '#C', '#D', '#E', '#F', '#G', '#H', '#I']
-}
-
-
-
 function disableAllClicks() {
-    disables.disableA.forEach(id => {
-        let cells = document.querySelectorAll(id);
+        let cells = document.querySelectorAll('.a');
         cells.forEach(cell => {
             cell.onclick = null;
         });
-    });
-}
+    };
+
+
 
 function enableAllClicks() {
-    let cells = document.querySelectorAll(id);
+    let cells = document.querySelectorAll('.a');
     cells.forEach(cell => {
         if (cell.innerHTML === '') {
             cell.onclick = function () {
@@ -100,5 +87,6 @@ function enableAllClicks() {
             };
         }
     });
-};
+}
+
 
